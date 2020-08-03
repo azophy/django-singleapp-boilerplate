ea12cccc842834e901c143c4d1404002c66ed4db
Abdurrahman Shofy Adianto <azophy@gmail.com>
2020-08-03 21:00:33 +0700

    add hello world for root path

--- app/config/urls.py
+++ app/config/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world")
 
 urlpatterns = [
+    path('', index),
+
     path('admin/', admin.site.urls),
 ]
