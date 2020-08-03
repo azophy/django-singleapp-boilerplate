a0bbbc8008a4a26347702d0d59f82479c7c71e4c
Abdurrahman Shofy Adianto <azophy@gmail.com>
2020-08-03 20:38:40 +0700

    Add settings for heroku deployment

--- README.md
+++ README.md
@@ -9,5 +9,12 @@ Adapted from article by Marios Zindilis ()https://zindilis.com/blog/2017/01/06/d
 - .env file support
 - heroku deploy ready
 
+## How To Use
+
+1. Download zip and extract
+2. Edit & save example.env as .env . The most important thing to set is the SECRET_KEY variable. Everything else is optional
+3. `pip install -r requirements.txt`
+4. `python manage.py runserver`
+5. To deploy to heroku, first make sure the remote source has been pointing to heroku. then just `git push heroku master`
 
 
--- app/config/settings.py
+++ app/config/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+APP_ENV = os.getenv('APP_ENV', 'PROD')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -119,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+if APP_ENV == 'PROD':
+    import django_heroku
+    django_heroku.settings(locals())
+
+
